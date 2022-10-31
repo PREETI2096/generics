cd8c71031ff94c0a1ec77673cb8bcd04140325ff
PREETI2096 <preetirg96@gmail.com>
2022-11-01 00:41:48 +0530

    [preeti]:added generics programs

--- src/com/bridgelabz/generics/Maximum.java
+++ src/com/bridgelabz/generics/Maximum.java
@@ -1,17 +1,16 @@
 package com.bridgelabz.generics;
 
 public class Maximum {
-	public void findMax(String x , String y , String  z) {
-		if(x.compareTo(y)>=0 && x.compareTo(z)>=0) {
-			System.out.println(x+" is maximum number.");
+	public < T extends Comparable<T>> void findMax(T n1 ,T n2,T n3 ) {
+		if(n1.compareTo(n2)>=0 && n1.compareTo(n3)>=0) {
+			System.out.println(n1+" is maximum number.");
 		}
-		else if(y.compareTo(x)>=0 && y.compareTo(z)>=0){
-		System.out.println(y+" is maximum");
+		else if(n2.compareTo(n1)>=0 && n2.compareTo(n3)>=0){
+		System.out.println(n2+" is maximum");
 		}
 		else {
-		System.out.println(z + " is maximum");
-	}	
-}
-
+		System.out.println(n3 + " is maximum");
+	}
+	}
 
 }
